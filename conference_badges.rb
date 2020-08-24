7317d83a0f6e91c62c1f152443bad35de98ab05b
Jason Beckstead <jason_beckstead@yahoo.com>
2020-08-24 16:38:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,4 @@
 def badge_maker(name)
-
-  name.each {|name| puts "Hello, my name is #{name}"}
+ name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  name.each {|name| puts "Hello, my name is #{name}."}
 end
