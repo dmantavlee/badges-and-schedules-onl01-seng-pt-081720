4d9c4a1bfd62d1c7b1a705d75d11e8cbfac84dad
Jason Beckstead <jason_beckstead@yahoo.com>
2020-08-24 16:49:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,9 @@
-name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def badge_maker(badge)
   "Hello, my name is #{name}."
+end
+def batch_badge_creator(attendees)
+  attendees.each do |attendee|
+    "Hello, my name is #{attendee}."
+  end
 end
